bf8b8a83fba4375f48ea4068bcde18cec585695c
Mark Hobson <markhobson@gmail.com>
2023-06-06 12:24:59 +0100

    Style with Bootstrap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,10 +6,20 @@
     ];
 </script>
 
-<h1>Contacts</h1>
+<nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1">Contacts</span>
+    </div>
+</nav>
 
-<ul>
-    {#each contacts as contact}
-        <li>{contact}</li>
-    {/each}    
-</ul>
+<div class="container-fluid g-0">
+    <div class="row g-0">
+        <div class="col-4">
+            <ul class="list-group list-group-flush">
+                {#each contacts as contact}
+                    <li class="list-group-item">{contact}</li>
+                {/each}    
+            </ul>        
+        </div>
+    </div>
+</div>
