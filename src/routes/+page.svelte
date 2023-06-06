99eb573d6ec802baaa8ed141957ca8c15782c01b
Mark Hobson <markhobson@gmail.com>
2023-06-06 11:57:37 +0100

    List contacts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let contacts = [
+        "Chip Smith",
+        "Randy Horn",
+        "Zane High"
+    ];
+</script>
+
+<h1>Contacts</h1>
+
+<ul>
+    {#each contacts as contact}
+        <li>{contact}</li>
+    {/each}    
+</ul>
