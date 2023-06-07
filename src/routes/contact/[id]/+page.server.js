e5a7923aee10a836a64b48fb5a7f6fcff0331980
Mark Hobson <markhobson@gmail.com>
2023-06-07 16:01:48 +0100

    Redirect to list if contact not found

--- src/routes/contact/[id]/+page.server.js
+++ src/routes/contact/[id]/+page.server.js
@@ -1,11 +1,17 @@
+import { redirect } from '@sveltejs/kit';
 import contacts from "$lib/server/contacts.js";
 
 export function load({ params }) {
     const id = parseInt(params.id);
+    const contact = contacts.get(id);
+
+    if (contact === undefined) {
+        throw redirect(303, "/");
+    }
 
     return {
         contacts: contacts.getAll(),
-        contact: contacts.get(id)
+        contact
     };
 }
 
